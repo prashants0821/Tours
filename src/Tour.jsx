2afe4ac0c38478740f86fd8bd44f01adf6fc47c1
Prashant <prashantsamant5@gmail.com>
2023-03-29 13:46:07 +0530

    created tours react page

---
+++ src/Tour.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+const Tour = ({ id, image, info, name, price, removeTours }) => {
+  const [readMore, SetReadMore] = useState(false);
+  console.log(info.substring(0, 10));
+
+  return (
+    <article className="single-tour">
+      <img src={image} alt={name} className="img" />
+      <span className="tour-price">${price}</span>
+      <div className="tour-info">
+        <h5>{name}</h5>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button
+            type="button"
+            className="info-btn"
+            onClick={() => SetReadMore(!readMore)}
+          >
+            {readMore ? "show less" : "read more"}
+          </button>
+        </p>
+        <button
+          type="button"
+          className="btn btn-block delete-btn"
+          onClick={() => removeTours(id)}
+        >
+          Not interested
+        </button>
+      </div>
+    </article>
+  );
+};
+export default Tour;
